375cd399965d8d1c6e0ba3a31132076c86c9d84d
tfedorov <t.fedorov.s@gmail.com>
2020-02-03 19:08:23 +0200

    Add gradle and postgres

---
+++ src/main/scala/com/tfedorov/SparkPostgressApp.scala
@@ -0,0 +1,24 @@
+package main.scala.com.tfedorov
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object SparkPostgressApp extends App  with Logging {
+
+  log.warn("*******Start*******")
+  private val targetPath = args.headOption
+  log.warn("Trying to save into " + targetPath)
+  if (targetPath.isEmpty)
+    System.exit(0)
+
+  private val session = SparkSession.builder.master("local")
+    .appName(this.getClass.getCanonicalName)
+    .getOrCreate()
+
+ val input: DataFrame =  session.read.format("jdbc")
+    .option("url", "jdbc:postgresql://localhost/tmp?user=postgres&password=Ckfdf1")
+    .option("query", "select hash, value from tmp.tmp_table")
+    .load()
+
+  input.show
+}
